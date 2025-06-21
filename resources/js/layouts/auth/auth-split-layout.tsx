f6fe99e1dfdbb9d2f91cd3802c3f1dff1f8f33bb
alifzaidan <zaidanalif22710@gmail.com>
2025-06-21 11:36:11 +0700

    update auth login img

--- resources/js/layouts/auth/auth-split-layout.tsx
+++ resources/js/layouts/auth/auth-split-layout.tsx
@@ -12,23 +12,22 @@ export default function AuthSplitLayout({ children, title, description }: PropsW
 
     return (
         <div className="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
-            <div className="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r">
-                <div className="absolute inset-0 bg-zinc-900" />
-                <Link href={route('home')} className="relative z-20 flex items-center text-lg font-medium">
-                    <img src="/assets/images/logo-secondary.png" alt="Aksademy" className="w-40 fill-current" />
-                </Link>
-                {quote && (
-                    <div className="relative z-20 mt-auto">
-                        <blockquote className="space-y-2">
-                            <p className="text-lg">&ldquo;{quote.message}&rdquo;</p>
-                            <footer className="text-sm text-neutral-300">{quote.author}</footer>
-                        </blockquote>
-                    </div>
-                )}
+            <div className="bg-muted relative hidden h-full flex-col overflow-hidden p-0 text-white lg:flex dark:border-r">
+                <img src="/assets/images/login-img.png" alt="Aksademy" className="absolute inset-0 z-0 h-full w-full object-cover object-center" />
+                <div className="relative z-10 flex h-full w-full flex-col p-10">
+                    {quote && (
+                        <div className="mt-auto">
+                            <blockquote className="space-y-2">
+                                <p className="text-lg">&ldquo;{quote.message}&rdquo;</p>
+                                <footer className="text-sm text-neutral-300">{quote.author}</footer>
+                            </blockquote>
+                        </div>
+                    )}
+                </div>
             </div>
             <div className="w-full lg:p-8">
                 <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-                    <Link href={route('home')} className="relative z-20 flex items-center justify-center lg:hidden">
+                    <Link href={route('home')} className="relative z-20 flex items-center justify-center">
                         <img src="/assets/images/logo-primary-2.png" alt="Aksademy" className="w-20 fill-current" />
                     </Link>
                     <div className="flex flex-col items-center gap-2 text-center">
