b3637c3f7b04a1a5e908fb0ebf8fc139132171e4
alifzaidan <zaidanalif22710@gmail.com>
2025-06-29 19:05:19 +0700

    update build

---
+++ public/build/assets/icon-CkmJFeD3.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-ehuvTaMD.js";import{c as t}from"./utils-9G-TgTw2.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
