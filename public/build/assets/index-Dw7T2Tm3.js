1c840e1eeed5720eba61870f7672e7f103818703
alifzaidan <zaidanalif22710@gmail.com>
2025-06-29 16:03:02 +0700

    update build

---
+++ public/build/assets/index-Dw7T2Tm3.js
@@ -0,0 +1,1 @@
+import{g as r,a}from"./app-CYQN4a4X.js";var t=a();const e=r(t);export{e as R,t as r};
---
+++ public/build/assets/use-motion-value-C6tL84-K.js
@@ -0,0 +1,1 @@
+import{r as t}from"./app-CYQN4a4X.js";import{u as a,M as u,c as r}from"./proxy-BCQxaIMf.js";function i(o){const e=a(()=>r(o)),{isStatic:s}=t.useContext(u);if(s){const[,n]=t.useState(o);t.useEffect(()=>e.on("change",n),[])}return e}export{i as u};
