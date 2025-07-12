2bf761dc7f76dce5d6602aaa651d31fb422e4e07
alifzaidan <zaidanalif22710@gmail.com>
2025-07-12 13:31:40 +0700

    add not found 404 page

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ use App\Http\Controllers\User\Profile\ProfileController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WebinarController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/course', [UserCourseController::class, 'index'])->name('course.index');
@@ -146,3 +147,7 @@ Route::middleware(['auth', 'verified', 'role:admin|mentor|affiliate'])->prefix('
 
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
+
+Route::fallback(function () {
+    return Inertia::render('errors/not-found');
+});
