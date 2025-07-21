0b7431c26cd19c3d07543cdaf44689bd40870bd7
alifzaidan <zaidanalif22710@gmail.com>
2025-07-21 10:23:06 +0700

    update build

--- public/build/assets/icon-D4mHZQrL.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-CQZnto6F.js";import{c as t}from"./utils-DyL2Jc6N.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
