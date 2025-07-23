bcb0746846c447afed8c2c7a5e37641a4f087405
alifzaidan <zaidanalif22710@gmail.com>
2025-07-23 14:15:10 +0700

    update build

--- public/build/assets/icon-BjrgqxNh.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-B4CLGjlu.js";import{c as t}from"./utils-CHmgok-F.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
