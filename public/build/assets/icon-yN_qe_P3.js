8f66ad26f799cb1a2267336928732c1a9375e877
alifzaidan <zaidanalif22710@gmail.com>
2025-08-26 10:32:19 +0700

    update build

---
+++ public/build/assets/icon-yN_qe_P3.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-B4Eyz-04.js";import{c as t}from"./utils-_M32tCIy.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
