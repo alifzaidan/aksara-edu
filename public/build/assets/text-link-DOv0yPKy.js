33c932a417684c98ae1ef00563b9d0fb9db91f1f
alifzaidan <zaidanalif22710@gmail.com>
2025-08-26 13:26:28 +0700

    update build

--- public/build/assets/text-link-DOv0yPKy.js
+++
@@ -1,1 +0,0 @@
-import{j as t,$ as n}from"./app-BKrwHhRF.js";import{c as a}from"./utils-DRxPZYFZ.js";function u({className:r="",children:e,...o}){return t.jsx(n,{className:a("text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current! dark:decoration-neutral-500",r),...o,children:e})}export{u as T};
