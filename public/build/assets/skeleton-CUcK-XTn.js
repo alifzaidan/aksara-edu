67ca54125ed05f5c7c8b25093bef9ee0bd41e25b
alifzaidan <zaidanalif22710@gmail.com>
2025-09-22 19:26:37 +0700

    update build

---
+++ public/build/assets/skeleton-CUcK-XTn.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-BIk08m1L.js";import{c as r}from"./utils-DGVEB0sE.js";function m({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:r("bg-primary/10 animate-pulse rounded-md",t),...e})}export{m as S};
