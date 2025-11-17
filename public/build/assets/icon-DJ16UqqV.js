4a0d64d5611d4c88bb210222cb2ff52dd955d913
alifzaidan <zaidanalif22710@gmail.com>
2025-11-17 11:20:24 +0700

    update build

---
+++ public/build/assets/icon-DJ16UqqV.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-B8seI4iJ.js";import{c as t}from"./utils-Ban7Nq7u.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
---
+++ public/build/assets/index-B5z3Gm-Z.js
@@ -0,0 +1,1 @@
+import{g as r,a}from"./app-B8seI4iJ.js";var t=a();const e=r(t);export{e as R,t as r};
