0b74fcc68ea4245ee177e133bcb571d9450d3403
alifzaidan <zaidanalif22710@gmail.com>
2025-11-25 18:48:28 +0700

    update build

---
+++ public/build/assets/icon-DHTUL2Q5.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-DcKGOM4z.js";import{c as t}from"./utils-Cguiwh0o.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
