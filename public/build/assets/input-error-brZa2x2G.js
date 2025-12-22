803dd93c64f46f65e77e4eb395b5ec25ecff3516
alifzaidan <zaidanalif22710@gmail.com>
2025-12-22 20:46:31 +0700

    update build

--- public/build/assets/input-error-brZa2x2G.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-B8hh7hbn.js";import{c as o}from"./utils-Ba6Capm0.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
