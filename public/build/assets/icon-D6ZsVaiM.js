e708397323409ea602f43ce512a5534614b91e12
alifzaidan <zaidanalif22710@gmail.com>
2025-12-23 09:12:34 +0700

    update build

--- public/build/assets/icon-D6ZsVaiM.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-CyRuYe4M.js";import{c as t}from"./utils-DkTViM1J.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
---
+++ public/build/assets/skeleton-Cum-WgQw.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-uqX9d6eK.js";import{c as r}from"./utils-BBlCXfg8.js";function m({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:r("bg-primary/10 animate-pulse rounded-md",t),...e})}export{m as S};
