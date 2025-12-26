6fb0c805948832fcda178101011c020156278260
alifzaidan <zaidanalif22710@gmail.com>
2025-12-26 13:16:07 +0700

    update build

--- public/build/assets/use-motion-value-DdNnu6nT.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./app-uqX9d6eK.js";import{u as a,M as u,c as r}from"./proxy-oMqYnMB5.js";function i(o){const e=a(()=>r(o)),{isStatic:s}=t.useContext(u);if(s){const[,n]=t.useState(o);t.useEffect(()=>e.on("change",n),[])}return e}export{i as u};
