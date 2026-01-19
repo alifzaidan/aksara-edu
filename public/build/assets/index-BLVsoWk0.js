3a477d9d278342753f55a184dc0b6143eecd9119
alifzaidan <zaidanalif22710@gmail.com>
2026-01-19 12:47:25 +0700

    update build

--- public/build/assets/index-BLVsoWk0.js
+++
@@ -1,1 +0,0 @@
-import{g as r,a}from"./app-Djq52izT.js";var t=a();const e=r(t);export{e as R,t as r};
--- public/build/assets/use-motion-value-DrOuiLon.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./app-Djq52izT.js";import{u as a,M as u,c as r}from"./proxy-D4yuY5dl.js";function i(o){const e=a(()=>r(o)),{isStatic:s}=t.useContext(u);if(s){const[,n]=t.useState(o);t.useEffect(()=>e.on("change",n),[])}return e}export{i as u};
