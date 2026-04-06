52df9fb6350e71ebe3dfd9106f7f738e5a297a53
bagussyahrijal <bagussyahrijal123@gmail.com>
2026-04-06 09:17:42 +0700

    modify axios in package and build

--- public/build/assets/input-error-PbyLT-yH.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-DWHOKDOq.js";import{c as o}from"./utils-Bismg2lm.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
