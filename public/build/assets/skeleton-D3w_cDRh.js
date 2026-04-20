783696440e9ecaa93077be253d4bf0724b64182d
alifzaidan <zaidanalif22710@gmail.com>
2026-04-20 09:38:12 +0700

    update build

--- public/build/assets/skeleton-D3w_cDRh.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-B22pEYuR.js";import{c as r}from"./utils-D-aIFOQ0.js";function m({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:r("bg-primary/10 animate-pulse rounded-md",t),...e})}export{m as S};
--- public/build/assets/use-motion-value-D0Xu2iOF.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./app-B22pEYuR.js";import{u as a,M as u,c as r}from"./proxy-D_gE0Myn.js";function i(o){const e=a(()=>r(o)),{isStatic:s}=t.useContext(u);if(s){const[,n]=t.useState(o);t.useEffect(()=>e.on("change",n),[])}return e}export{i as u};
