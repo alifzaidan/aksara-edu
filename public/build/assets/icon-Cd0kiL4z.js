f56dc9be09f91d057f185bd08b65245ce3638ff7
bagussyahrijal <bagussyahrijal123@gmail.com>
2026-05-23 14:40:39 +0700

    update build

---
+++ public/build/assets/icon-Cd0kiL4z.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-CN1pV84L.js";import{c as t}from"./utils-t6fyTf68.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
