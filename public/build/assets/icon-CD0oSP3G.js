767ce254231919932be55cf0b6ee06254ddadc78
alifzaidan <zaidanalif22710@gmail.com>
2026-05-31 11:55:34 +0700

    update build

--- public/build/assets/icon-CD0oSP3G.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-Bg9FKFFJ.js";import{c as t}from"./utils-BxIa1riY.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
--- public/build/assets/use-motion-value-CArveVUV.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./app-Bg9FKFFJ.js";import{u as a,M as u,c as r}from"./proxy-B9ArCULB.js";function i(o){const e=a(()=>r(o)),{isStatic:s}=t.useContext(u);if(s){const[,n]=t.useState(o);t.useEffect(()=>e.on("change",n),[])}return e}export{i as u};
