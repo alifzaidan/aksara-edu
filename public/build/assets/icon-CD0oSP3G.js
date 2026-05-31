3b4be5b32536c306d048ebad313b44e57b8df56f
alifzaidan <zaidanalif22710@gmail.com>
2026-05-31 10:58:15 +0700

    update build

---
+++ public/build/assets/icon-CD0oSP3G.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-Bg9FKFFJ.js";import{c as t}from"./utils-BxIa1riY.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
