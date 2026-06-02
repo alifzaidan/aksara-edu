0a6093dafef63b338a54bd0d1486195c60a23c35
alifzaidan <zaidanalif22710@gmail.com>
2026-06-02 09:05:09 +0700

    update build

---
+++ public/build/assets/use-motion-value-BBwlhxqf.js
@@ -0,0 +1,1 @@
+import{r as t}from"./app-PxlntORd.js";import{u as a,M as u,c as r}from"./proxy-CUQ5-mxe.js";function i(o){const e=a(()=>r(o)),{isStatic:s}=t.useContext(u);if(s){const[,n]=t.useState(o);t.useEffect(()=>e.on("change",n),[])}return e}export{i as u};
