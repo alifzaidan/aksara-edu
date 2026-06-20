427b555e13ce749dea7fc9ffd7d6efd7590bb9f2
alifzaidan <zaidanalif22710@gmail.com>
2026-06-20 08:39:58 +0700

    update build

--- public/build/assets/icon-0uEmqpC_.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-De8POVH2.js";import{c as t}from"./utils-DPAeKuug.js";function a({iconNode:o,className:r,...s}){return n.jsx(o,{className:t("h-4 w-4",r),...s})}export{a as I};
