48e2e199517bdaa2a90ef5af57300af053061595
alifzaidan <zaidanalif22710@gmail.com>
2026-07-20 09:19:37 +0700

    update build

--- public/build/assets/skeleton-CKOS2d7A.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-B6qcVDtL.js";import{c as r}from"./utils-CFXma4ds.js";function m({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:r("bg-primary/10 animate-pulse rounded-md",t),...e})}export{m as S};
